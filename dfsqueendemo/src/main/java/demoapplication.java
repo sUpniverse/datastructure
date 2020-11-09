12a13db56e931cf183e764f8d95d381f34e288ff
supniverse <kmsup2@gmail.com>
2020-11-09 23:58:37 +0900

    add stack

---
+++ dfsqueendemo/src/main/java/demoapplication.java
@@ -0,0 +1,8 @@
+public class demoapplication {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(1 || 2);
+    }
+}
---
+++ stack/src/Stack.java
@@ -0,0 +1,60 @@
+public class Stack {
+
+    private Object[] objects;
+    private int size;
+    private int front;
+
+    public Stack(int size) {
+        this.size = size;
+        this.front = 0;
+        this.objects = new Object[size];
+    }
+
+    public Stack() {
+        this.size = 10;
+        this.front = 0;
+        this.objects = new Object[size];
+    }
+
+    public void push(Object object) {
+        if(front >= size-1) {
+            throw new StackOverflowError();
+        }
+        objects[front++] = object;
+        System.out.println("Push Data : " + object.toString());
+    }
+
+    public Object pop() {
+        Object object = peek();
+        objects[--front] = null;
+        System.out.println("Pop Data : " + object.toString());
+        return object;
+    }
+
+    public Object peek(){
+        if(isEmpty()) {
+            throw new NullPointerException();
+        }
+        return objects[front-1];
+    }
+
+    public boolean isEmpty(){
+        return this.front <= 0;
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+
+
+
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+    }
+
+}
